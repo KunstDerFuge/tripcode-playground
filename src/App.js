d4f8f37d1891bd4b33693ebba0b7f84d3e0899e1
Robert Amour <robert@ferretech.io>
2022-07-08 16:21:44 -0700

    Add default values and make fields narrower

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Base64 from 'crypto-js/enc-base64'
 import CryptoJS from 'crypto-js'
 
 function App() {
-  const [salt, setSalt] = React.useState('')
-  const [trip1, setTrip1] = React.useState('')
-  const [trip2, setTrip2] = React.useState('')
-  const [trip3, setTrip3] = React.useState('')
+  const [salt, setSalt] = React.useState('secret_salt')
+  const [trip1, setTrip1] = React.useState('pass1')
+  const [trip2, setTrip2] = React.useState('pass2')
+  const [trip3, setTrip3] = React.useState('pass3')
 
   // I am so tired this function is just copypasted from StackOverflow
   // https://stackoverflow.com/a/37468518
@@ -29,33 +29,30 @@ function App() {
   }
 
   return (
-    <div className="App" style={{display: 'flex', flexDirection: 'column'}}>
+    <div className="App" style={{display: 'flex', flexDirection: 'column', width: '725px'}}>
       <div>
-        <TextField sx={{m: 4}} name="salt" label="Secret Salt" value={salt} onChange={e => setSalt(e.target.value)}/>
+        <TextField sx={{m: 4, width: '16ch'}} name="salt" label="Secret Salt" value={salt} onChange={e => setSalt(e.target.value)}/>
       </div>
       <div style={{display: 'flex', alignItems: 'center', alignSelf: 'center'}}>
-        <TextField sx={{m: 1}} name="trip1" label="Trip Password 1" value={trip1}
+        ##<TextField sx={{m: 1, width: '16ch'}} name="trip1" label="Trip Password 1" value={trip1}
                    onChange={e => setTrip1(e.target.value)}/>
         =>
-        <TextField sx={{m: 1}} label="Pass1 + Salt" value={trip1 + salt}/>
-        =>
-        &nbsp;!!<TextField sx={{m: 1}} label="Tripcode 1" value={generate_tripcode(trip1, salt)}/>
+        <TextField sx={{m: 1, width: '18ch'}} label="Pass1 + Salt" value={trip1 + salt}/>
+        =>&nbsp;&nbsp;!!<TextField sx={{m: 1, width: '16ch'}} label="Tripcode 1" value={generate_tripcode(trip1, salt)}/>
       </div>
       <div style={{display: 'flex', alignItems: 'center', alignSelf: 'center'}}>
-        <TextField sx={{m: 1}} name="trip2" label="Trip Password 2" value={trip2}
+        ##<TextField sx={{m: 1, width: '16ch'}} name="trip2" label="Trip Password 2" value={trip2}
                    onChange={e => setTrip2(e.target.value)}/>
         =>
-        <TextField sx={{m: 1}} label="Pass2 + Salt" value={trip2 + salt}/>
-        =>
-        &nbsp;!!<TextField sx={{m: 1}} label="Tripcode 2" value={generate_tripcode(trip2, salt)}/>
+        <TextField sx={{m: 1, width: '18ch'}} label="Pass2 + Salt" value={trip2 + salt}/>
+        =>&nbsp;&nbsp;!!<TextField sx={{m: 1, width: '16ch'}} label="Tripcode 2" value={generate_tripcode(trip2, salt)}/>
       </div>
       <div style={{display: 'flex', alignItems: 'center', alignSelf: 'center'}}>
-        <TextField sx={{m: 1}} name="trip3" label="Trip Password 3" value={trip3}
+        ##<TextField sx={{m: 1, width: '16ch'}} name="trip3" label="Trip Password 3" value={trip3}
                    onChange={e => setTrip3(e.target.value)}/>
         =>
-        <TextField sx={{m: 1}} label="Pass3 + Salt" value={trip3 + salt}/>
-        =>
-        &nbsp;!!<TextField sx={{m: 1}} label="Tripcode 3" value={generate_tripcode(trip3, salt)}/>
+        <TextField sx={{m: 1, width: '18ch'}} label="Pass3 + Salt" value={trip3 + salt}/>
+        =>&nbsp;&nbsp;!!<TextField sx={{m: 1, width: '16ch'}} label="Tripcode 3" value={generate_tripcode(trip3, salt)}/>
       </div>
     </div>
   )
